5397439ea0c07835fdff21f15501bc3f44cf4e97
Henry Macfarlane <hjemacfarlane@gmail.com>
2020-06-24 15:18:31 +0100

    header added with styling

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import {
   BrowserRouter as Router,
   Route
 } from "react-router-dom";
+import Header from "../src/Header/Header"
 
 const App = () => {
   return (
     <Router>
       <React.Fragment>
-        <h1>Hello World</h1>
+        <Header />
       </React.Fragment>
     </Router>
   );
