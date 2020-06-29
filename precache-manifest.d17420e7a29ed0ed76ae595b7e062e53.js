d40e9b58a53b7fd1ff85b3807e3ccb1f5a1803b9
Henry Macfarlane <hjemacfarlane@gmail.com>
2020-06-29 21:03:07 +0100

    Updates

---
+++ precache-manifest.d17420e7a29ed0ed76ae595b7e062e53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b8bd09f828dc6e758f156bef042fbc",
+    "url": "/techChallenge/index.html"
+  },
+  {
+    "revision": "3b24fe540e8fd852abd6",
+    "url": "/techChallenge/static/css/main.7d4b3a08.chunk.css"
+  },
+  {
+    "revision": "8106a00478d9b3423d4f",
+    "url": "/techChallenge/static/js/2.31dc79ff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/techChallenge/static/js/2.31dc79ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b24fe540e8fd852abd6",
+    "url": "/techChallenge/static/js/main.d6d62add.chunk.js"
+  },
+  {
+    "revision": "c32934d0077b3c95e771",
+    "url": "/techChallenge/static/js/runtime-main.01fc36a5.js"
+  },
+  {
+    "revision": "dc386f0b36c8cfdbe2f0efbbb5c83781",
+    "url": "/techChallenge/static/media/Notable-Regular.dc386f0b.ttf"
+  }
+]);
