2cee2db2d50b2577e9ed65db8d45427e8049b45e
Henry Macfarlane <hjemacfarlane@gmail.com>
2020-07-06 14:42:45 +0100

    logic for splitting into two teams added

---
+++ src/components/Team/Team.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Team = () => {
+    return (
+        <>
+            <header className="header">
+                <h1 className="h1">Select Your Team</h1>
+            </header>
+        </>
+    )
+}
+
+export default Team; 
